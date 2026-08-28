7fc5279d1b1298365af57036a72dbf341cd591ff
Angel Gabriel Garcia <ag3224308@gmail.com>
2026-08-27 23:32:41 -0700

    Initial Commit

---
+++ MeetXcode/MeetXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetXcodeApp.swift
+//  MeetXcode
+//
+//  Created by Angel Gabriel Garcia  on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct MeetXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
